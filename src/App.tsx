970b7a9edb82920dafce2340845bd1f27a1f7619
256hax <256hax@gmail.com>
2023-03-21 13:40:25 +0900

    docs: minor change

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import './App.css';
 import { useLayoutEffect } from 'react'
 import { WalletAdapter } from './components/WalletAdapter';
 
+// Register your wallet.
 import { addWindow } from './components/wallet/window';
 
 function App() {
   const mount = () => {
     const unmount = () => {
-      // Add Solana Wallet to Window object.
+      // Add your wallet to Window object.
       addWindow();
     }
     return unmount
